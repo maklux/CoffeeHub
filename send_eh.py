f7acb56db2c70b28e1bc76aff83c1b3aac1781f3
mak <martin0kayser@gmail.com>
2018-05-11 00:27:05 +0200

    minor fix, added comments

--- send_eh.py
+++ send_eh.py
@@ -5,24 +5,28 @@ import time
 from gpiozero import LED
 from azure.servicebus import ServiceBusService
 
+# Connection strings for Event Hub
 key_name = 'mysecretkeyname'
 key_value = 'mysecretkeyvalue'
-service_namespace = 'eventhub namespace'
-eventhub_name = 'eventhub name'
-interval = 30 #seconds
-sensor1 = "28-041700305eff"
-sensor2 = "28-0517001488ff"
-led_red = LED(17)
-led_green = LED(18)
- 
+service_namespace = 'coffeeevents'
+eventhub_name = 'pavoni'
+
+# Establish connection to Event Hub
 host = socket.gethostname()
 sbs = ServiceBusService(service_namespace,
 shared_access_key_name=key_name,
 shared_access_key_value=key_value)
 sbs.create_event_hub(eventhub_name)
 
+# Parameters
+interval = 10 #seconds
+sensor1 = "28-041700305eff"
+sensor2 = "28-0517001488ff"
+led_red = LED(17)
+led_green = LED(18)
 
-# Fetch data from sensors
+
+# Fetch values from sensors
 def read_temp(id):
     try:
         with open("/sys/bus/w1/devices/" + id + "/w1_slave") as f:
@@ -34,10 +38,13 @@ def read_temp(id):
     return temp
 
 while True:
+    # Read sensor values
     temp1 = read_temp(sensor1)
     temp2 = read_temp(sensor2)
+    # Create timestamp
     unix = int(time.time())
     
+    # Flag for espresso temperature reached
     if temp1 > 90:
         ready = True
         led_green.on()
@@ -45,6 +52,7 @@ while True:
         ready = False
         led_green.off()
 
+    # Flag for when machine is on
     if temp1 > 30:
         on = True
         led_red.on()
@@ -52,9 +60,10 @@ while True:
         on = False
         led_red.off()
 
-    data = {'Hostname': host, 'Timestamp': unix, 'Temperature1': temp1, 'Temperature2': temp2, 'On': on 'Ready': ready}
+    # Package data
+    data = {'Hostname': host, 'Timestamp': unix, 'Temperature1': temp1, 'Temperature2': temp2, 'On': on, 'Ready': ready}
     msg = json.dumps(data)
     print(msg)
+    # Send to event hub
     sbs.send_event(eventhub_name, msg)
-
     time.sleep(interval)
