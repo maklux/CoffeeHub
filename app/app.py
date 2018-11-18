e05b2a5ffd8f9d80a235baa1976eba62ff06f780
Martin Kayser <makayser@microsoft.com>
2018-11-18 17:55:26 +0100

    fully functional v1

--- app/app.py
+++ app/app.py
@@ -6,6 +6,7 @@ from rpi_rf import RFDevice
 app = Flask(__name__)
 
 def switch(code):
+    print('')
     rfdevice = RFDevice(17)
     rfdevice.enable_tx()
     rfdevice.tx_code(code, 1, 350)
@@ -13,38 +14,32 @@ def switch(code):
 
 @app.route('/')
 def welcome():
-    return 'Hello to Connected Coffee v0.1 - (c) MAK 2018'
+    return render_template('home.html',message='Hello to Connected Coffee v0.1 - (c) MAK 2018')
 
-@app.route('/on')
-def on():
-    switch(4474193)
-    message = "Pavoni just turned on. Enjoy your coffee!"
-    action="Turn Off"
-    return render_template('on.html', message=message, action=action)
+@app.route('/<device>/<action>')
+def action(device, action):
+    if action == 'on':
+        switch(4474193)
+        message = device + " just turned on. Enjoy your coffee!"
+        action_text = "Turn Off"
+    elif action == 'off':
+        switch(4474196)
+        message = device + ' just turned off. Give it some time to cool down. '
+        action_text = "Turn On"
+    else:
+        message = 'Action not recognized. Please try again. Currently the following actions are available: on, off.'
+        action_text = "Turn On"
 
-@app.route('/off')
-def off():
-    switch(4474196)
-    message='Pavoni just turned off. Give it some time to cool down. '
-    action="Turn On"
-    return render_template('on.html', message=message, action=action)
+    template_data = {
+        'action' : action_text,
+        'message' : message
+    }
+    return render_template('action.html', **template_data)
 
-@app.route('/test')
-def load_input():
-    # Load variables
-    pwd = request.args.get('pwd')
-    action = request.args.get('action')
-
-    # Resolve variables
-    if pwd == 'mak':
-        if action is None:
-            res = 'Error: No action Provided'
-        else:
-            res = action
-    else: 
-        res = 'Error: Login failed'
-
-    return res
+@app.route('/pipe')
+def pipe():
+    act = request.args.get("action")
+    return action('mak',act)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080)
